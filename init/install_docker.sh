5cfa6a88150159872e69e3b6d4259fd8b0830bbd
kyakya <caliburn1994@gmail.com>
2020-11-16 08:01:55 -0800

    install docker

---
+++ init/install_docker.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# https://docs.docker.com/engine/install/ubuntu/
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# test
+sudo docker run hello-world
+
+
+# add the current user to docker group
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# expose docker to tcp://127.0.0.1:2375
+# https://docs.docker.com/engine/install/linux-postinstall/
+sudo tee /usr/lib/systemd/system/docker.service <<-'EOF'
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd -H unix:// -H tcp://127.0.0.1:2375
+EOF
+sudo systemctl daemon-reload
+
+newgrp docker
+docker run hello-world
