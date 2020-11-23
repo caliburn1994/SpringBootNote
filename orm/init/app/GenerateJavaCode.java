e607e4ffbda8f79f2b58d5ef60ab2e1f3aab948d
kyakya <caliburn1994@gmail.com>
2020-11-24 00:54:21 +0900

    orm mybatis

---
+++ orm/init/app/GenerateJavaCode.java
@@ -0,0 +1,49 @@
+package icu.kyakya.orm;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * https://mybatis.org/generator/running/runningWithJava.html
+ */
+public class GenerateJavaCode {
+    ClassPathResource generatorConfig_simple = new ClassPathResource("mybatis/generatorConfig_simple.xml");
+    ClassPathResource generatorConfig_dynamicSql = new ClassPathResource("mybatis/generatorConfig_dynamicSql.xml");
+
+
+    @Test
+    public void run_dynamicSQL() throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<>();
+        boolean overwrite = true;
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(generatorConfig_dynamicSql.getFile());
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+    @Test
+    public void run_simple() throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<>();
+        boolean overwrite = true;
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(generatorConfig_simple.getFile());
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+}
