64b6809b1ff3bd0935deed25decc19bb08957db8
kyakya <caliburn1994@gmail.com>
2020-12-12 19:23:13 +0900

    gradle

--- gradle.mk
+++ gradle.mk
@@ -2,3 +2,16 @@
 
 wrapper:
 	gradle wrapper
+
+gradle_build:
+	echo "skip test"
+	./gradlew build --exclude-task test
+
+
+gradle_help:
+	#echo "skip test"
+	#./gradlew build --exclude-task test
+
+
+sub_task:
+	./gradlew :other
