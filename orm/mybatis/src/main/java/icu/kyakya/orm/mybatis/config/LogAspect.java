34db8ffc9a91201dfe1c54da9d2b06762f25ef5d
kyakya <caliburn1994@gmail.com>
2020-12-22 00:11:21 +0900

    mybatis spring aop logger

---
+++ orm/mybatis/src/main/java/icu/kyakya/orm/mybatis/config/LogAspect.java
@@ -0,0 +1,30 @@
+package icu.kyakya.orm.mybatis.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Slf4j
+@Configuration
+public class LogAspect {
+
+    @Around("execution(* icu.kyakya.orm.mybatis.service..*(..))")
+    public Object entryAndExit(ProceedingJoinPoint pjp) throws Throwable {
+        Signature signature = pjp.getSignature();
+        String declaringTypeName = signature.getDeclaringTypeName();
+        String[] tmp = declaringTypeName.split("\\.");
+        String basename = tmp[tmp.length - 1];
+
+        log.info("[{}.{}] Start", basename, pjp.getSignature().getName());
+        Object result = pjp.proceed();
+        log.info("[{}.{}] Finish", basename, pjp.getSignature().getName());
+
+        return result;
+    }
+
+
+}
