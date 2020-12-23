11d98e29d47b781adaff205053688ddf2c720859
kyakya <caliburn1994@gmail.com>
2020-12-24 00:54:55 +0900

    update

--- web/SpringMVC/src/main/java/icu/kyakya/SpringMVC/SpringMvcApplication.java
+++ web/SpringMVC/src/main/java/icu/kyakya/SpringMVC/SpringMvcApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(MybatisConfig.class)
+
 public class SpringMvcApplication {
 
 	public static void main(String[] args) {
