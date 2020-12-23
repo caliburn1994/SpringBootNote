a295a76b205bfc9166b76316a368822102bdeb79
Chen Xiaojie <caliburn1994@gmail.com>
2020-12-23 11:21:16 +0900

    update redirect

--- web/SpringMVC/src/main/java/icu/kyakya/SpringMVC/controller/Example.java
+++ web/SpringMVC/src/main/java/icu/kyakya/SpringMVC/controller/Example.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -40,26 +41,45 @@ public class Example {
 
 
     /**
-     * @param modelMap modelMap包含了  commonModelAttribute()和exampleModel()的数据，
-     *                 但是由于form提交了一个ExampleModel，所以覆盖了和exampleModel()的ExampleModel
-     *                 '@Valid' 用于启动校验器
-     * @param result 用于处理校验器结果以及错误 todo
+     * @param modelMap           modelMap包含了  commonModelAttribute()和exampleModel()的数据，
+     *                           但是由于form提交了一个ExampleModel，所以覆盖了和exampleModel()的ExampleModel
+     *                           '@Valid' 用于启动校验器
+     * @param bindingResult      用于处理校验器结果以及错误 todo
+     * @param redirectAttributes 用于转发数据。
+     *                           addFlashAttribute()将可存储对象，数据将会被存在session中，数据仅存活一个request。
+     *                           addAttribute()将以为 request parameters形式存储数据，如果使用的是get，则将在url中添加 ?key=value&&key=value
+     *                           ref: https://stackoverflow.com/questions/14470111/spring-redirectattributes-addattribute-vs-addflashattribute
      */
     @PostMapping("/submit")
-    public String submit(ModelMap modelMap, @Valid @ModelAttribute("example") ExampleModel example, BindingResult result) {
+    public String submit(ModelMap modelMap, @Valid @ModelAttribute("example") ExampleModel example
+            , BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         log.info("提交后，将返回信息到index,并打印出来");
 
+        printModelMap(modelMap);
+
+        bindingResult.getAllErrors().forEach(err -> log.warn(err.getDefaultMessage()));
+
+        redirectAttributes.addFlashAttribute(example); //todo flash vs no flash
+        return "redirect:/example/index2"; //todo
+    }
+
+
+    @GetMapping("/index2")
+    public String index2(ModelMap modelMap, @ModelAttribute("example") ExampleModel example) {
+        log.info("已转发至 /example/index2");
+        printModelMap(modelMap);
+        return "example/index";
+    }
+
+
+    //todo 会话
+    // todo escape html?
+
+    private void printModelMap(ModelMap modelMap) {
         log.info("ModelMap拥有以下内容:");
         modelMap.forEach((k, v) -> {
             log.info("[key]:{} [value]:{}", k, v);
         });
-
-        result.getAllErrors().forEach(err->log.warn(err.getDefaultMessage()));
-
-        return "redirect:/example/index"; //todo
     }
-
-    //todo 会话
-
 
 }
