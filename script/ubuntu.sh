156189b95c49cd673aefef1330cbfbb4a669eaf6
Chen Xiaojie <caliburn1994@gmail.com>
2021-01-12 11:36:55 +0900

    update

---
+++ script/ubuntu.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# ubuntu 20
+
+# for logitech keyboard and mouse
+sudo apt-get install solaar
+sudo apt-get install solaar-gnome
+
+# git
+sudo apt-get install git
+
+# ssh
+sudo apt install openssh-server
