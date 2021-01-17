fdb3e1c8ea0516878bdd0da33247e14d1c8e1b71
kyakya <caliburn1994@gmail.com>
2021-01-17 22:30:04 +0900

    update script

--- script/init_spring_project/exec_history.sh
+++ script/init_spring_project/exec_history.sh
@@ -2,3 +2,4 @@
 
 #bash ./init_project.sh -m mvc -l web -d lombok,devtools,configuration-processor,testcontainers,web,thymeleaf
 #bash ./init_project.sh -m mybatis -l orm -d lombok,devtools,configuration-processor,mysql,mybatis,testcontainers,h2
+#bash ./init_project.sh -m jooq -l orm -d lombok,devtools,configuration-processor,h2,jooq,testcontainers
