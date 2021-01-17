21f62aee4eac22da73601bfcb8636f35150e93f0
kyakya <caliburn1994@gmail.com>
2021-01-17 20:53:44 +0900

    update script

--- script/init_spring_project/init_project.sh
+++ script/init_spring_project/init_project.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+. "../install/java.sh"
+
 root_dir="$(cd ../../; pwd)"
 
 . "${root_dir}/script/color.sh"
---
+++ script/install/java.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -e
+
+source "$HOME/.sdkman/bin/sdkman-init.sh"  # as login shell, because sdkman is just valid in login shell mode
+
+install() {
+  if ! type -p sdk &>/dev/null; then
+    echo "Installing sdk..."
+    # https://sdkman.io/install
+    curl -s "https://get.sdkman.io" | bash
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
+    sdk version
+  fi
+
+  if ! type -p java &>/dev/null; then
+    java_version=${1-"15.0.1-amzn"}
+    echo "Installing java..."
+    echo "java version: ${java_version}"
+    sdk install java "${java_version}"
+  fi
+
+  if ! type -p gradle &>/dev/null; then
+    echo "Installing latest gradle..."
+    sdk install gradle
+  fi
+
+  if ! type -p mvn &>/dev/null; then
+    echo "Installing latest gradle..."
+    sdk install maven
+  fi
+}
+
+echo "checking java..."
+
+if [ ! -d ~/.sdkman ]; then
+  install "$@"
+fi
