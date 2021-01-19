ac5bbed296eea9a0b7cef5063ad6713c88f4c648
kyakya <caliburn1994@gmail.com>
2021-01-19 21:52:44 +0900

    update script

--- script/deploy/k8s/minikube.sh
+++ script/deploy/k8s/minikube.sh
@@ -21,7 +21,10 @@ if ! type -p minikube &>/dev/null; then
   curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
   sudo dpkg -i minikube_latest_amd64.deb
   rm minikube_latest_amd64.deb
+
+  minikube addons enable ingress
 fi
 
 minikube start
+minikube dashboard
 
