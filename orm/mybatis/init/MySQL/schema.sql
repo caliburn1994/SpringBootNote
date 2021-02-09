d9d37d8d0d6e21da14cb33a23f7683be1b329b69
Chen Xiaojie <caliburn1994@gmail.com>
2021-02-09 23:30:33 +0900

    fix

--- orm/mybatis/init/MySQL/schema.sql
+++ orm/mybatis/init/MySQL/schema.sql
@@ -15,7 +15,7 @@ create table if not exists user
         primary key,
     name varchar(255) null,
     gender varchar(255) null,
-    height in  null,
+    height int  null,
     city_id int null,
     constraint user_city_city_id_fk
         foreign key (city_id) references city (city_id)
