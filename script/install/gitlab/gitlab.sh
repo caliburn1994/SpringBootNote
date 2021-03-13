c2ba34fa604bd28ea4930db474c320a5271e304c
Chen Xiaojie <caliburn1994@gmail.com>
2021-03-14 01:09:30 +0900

    extract configuration from gitlab.sh

--- script/install/gitlab/gitlab.sh
+++ script/install/gitlab/gitlab.sh
@@ -16,68 +16,10 @@ if ! kubectl get pods | grep gitlab &>/dev/null; then
   # - $(kubectl get nodes -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}')
   # - $(minikube ip)
   ip="$(minikube ip)"
+  export ip
 
   config_file="${SpringBootNote_path}/config/gitlab.yaml"
-  cat <<EOF >"$config_file"
-# values-minikube.yaml
-# This example intended as baseline to use Minikube for the deployment of GitLab
-# - Services that are not compatible with how Minikube runs are disabled
-# - Configured to use 192.168.99.100, and nip.io for the domain
-
-# Minimal settings
-global:
-  ingress:
-    configureCertmanager: false
-    class: "nginx"
-  hosts:
-    domain: "${ip}.nip.io"
-    externalIP: "${ip}"
-  rails:
-    bootsnap:
-      enabled: false
-  shell:
-    # Configure the clone link in the UI to include the high-numbered NodePort
-    # value from below (gitlab.gitlab-shell.service.nodePort)
-    port: 32022
-  psql:
-    host: ${POSTGRES_K8S_SERVICE}
-    database: postgres
-    username: postgres
-    password:
-      secret: ${POSTGRES_K8S_SERVICE}
-      key: postgresql-password
-# Don't use certmanager, we'll self-sign
-certmanager:
-  install: false
-# Use the "ingress" addon, not our Ingress (can't map 22/80/443)
-nginx-ingress:
-  enabled: false
-# Save resources, only 3 CPU
-prometheus:
-  install: false
-gitlab-runner:
-  install: false
-# Reduce replica counts, reducing CPU & memory requirements
-gitlab:
-  webservice:
-    minReplicas: 1
-    maxReplicas: 1
-  sidekiq:
-    minReplicas: 1
-    maxReplicas: 1
-  gitlab-shell:
-    minReplicas: 1
-    maxReplicas: 1
-    # Map gitlab-shell to a high-numbered NodePort to support cloning over SSH since
-    # Minikube takes port 22.
-    service:
-      type: NodePort
-      nodePort: 32022
-registry:
-  hpa:
-    minReplicas: 1
-    maxReplicas: 1
-EOF
+  envsubst < "${SpringBootNote_path}/config/gitlab-template.yaml" > "${config_file}"
 
   helm upgrade --install gitlab gitlab/gitlab \
     --timeout 600s \
@@ -89,14 +31,17 @@ EOF
   #    --set global.psql.database.secret=postgres \
   #    --set global.psql.password.key=postgresql-password \
 
-  # shellcheck disable=SC2154
-  echo "${pink}user: root"
-  echo "password: $(
+
+  cat << EOF > "${SpringBootNote_path}/config/bin/open-gitlab.sh"
+#!/usr/bin/env bash
+echo "user: root"
+echo password: $(
     kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode
     echo
-  )"
-  # shellcheck disable=SC2154
-  echo "site: https://gitlab.${ip}.nip.io ${reset}"
+  )
+xdg-open https://gitlab.${ip}.nip.io
+EOF
+
 fi
 
 # check gitlab if normal
