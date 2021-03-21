e6199d0a41f05f17ed9f314f1def630778e0003e
Chen Xiaojie <caliburn1994@gmail.com>
2021-03-21 16:35:32 +0900

    specific error handler

--- web/mvc/src/main/java/icu/kyakya/mvc/config/GlobalControllerExceptionHandler.java
+++ web/mvc/src/main/java/icu/kyakya/mvc/config/GlobalControllerExceptionHandler.java
@@ -1,13 +1,22 @@
 package icu.kyakya.mvc.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
+
+/**
+ * example: https://spring.io/blog/2013/11/01/exception-handling-in-spring-mvc
+ * <p>
+ * see here {@link org.springframework.web.bind.annotation.ExceptionHandler}, you can know what parameters can be used.
+ */
 @ControllerAdvice
 @Slf4j
 class GlobalControllerExceptionHandler {
@@ -18,14 +27,21 @@ class GlobalControllerExceptionHandler {
      */
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoHandlerFoundException.class)
-    public String handle404(WebRequest request) {
+    public String handle404(Model model, HttpServletRequest req, Exception ex) {
         return "/exceptions/404";
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({SQLException.class, DataAccessException.class})
+    public String databaseError(HttpServletRequest req, Exception ex) {
+        log.warn(ex.getMessage());
+        return "/exceptions/500";
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public String handle500(Exception exception) {
-        log.warn(exception.getMessage());
+    public String handle500(HttpServletRequest req, Exception ex) {
+        log.warn(ex.getMessage());
         return "/exceptions/500";
     }
 
--- web/mvc/src/main/java/icu/kyakya/mvc/controller/error/ErrorController.java
+++ web/mvc/src/main/java/icu/kyakya/mvc/controller/error/ErrorController.java
@@ -4,19 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.sql.SQLException;
+
 @Controller
 public class ErrorController {
 
     @GetMapping(value = "/error/1")
-    public String error(Model model) throws Exception {
-//        if (model != null) {
-//            throw new Exception();
-//        }
+    public String error1(Model model) throws Exception {
         int arr[] = new int[5];
         arr[9] = 250;
-
-
         return "/error/1";
     }
 
+
+    @GetMapping(value = "/error/2")
+    public String error2(Model model) throws SQLException {
+        if (model != null) {
+            throw new SQLException("SQLException happens!");
+        }
+        return "/error/2";
+    }
 }
