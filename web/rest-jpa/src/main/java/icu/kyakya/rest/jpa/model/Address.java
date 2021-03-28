1eeb11274a4aaef627a6173e1059eaba007add58
Chen Xiaojie <caliburn1994@gmail.com>
2021-03-28 16:19:27 +0900

    createdAt and updateAt field

--- web/rest-jpa/src/main/java/icu/kyakya/rest/jpa/model/Address.java
+++ web/rest-jpa/src/main/java/icu/kyakya/rest/jpa/model/Address.java
@@ -2,12 +2,14 @@ package icu.kyakya.rest.jpa.model;
 
 import icu.kyakya.rest.jpa.model.audit.AuditMetadata;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
 public class Address extends AuditMetadata {
